6ae72746f35be125b8f26d6b36bc334aa8992380
remilkies <160505182+remilkies@users.noreply.github.com>
2025-09-30 13:38:10 +0200

    individual movie template
    - css+html for individual movie page
    -replaces the leftButton so it fits the height of the other half
    - made watchButton change to watched on click in js.script
    - added watchedButton so it fits size with watchButton

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-    //ICONS HOVER + CLICK ACTIVE + TOGGLER
+    //BOOKMARK, WISHLIST, ICON HOVER + CLICK ACTIVE + TOGGLER
     const iconBookmarks = document.querySelectorAll(".watchlistIcon");
 
     iconBookmarks.forEach((iconBookmark) => {
@@ -21,6 +21,18 @@
       isActive = !isActive;
       iconBookmark.src = isActive ? iconBookmark.dataset.active : iconBookmark.dataset.default;
     });
+    });
+
+    // this is for the watch button toggled to watchedButton
+    const watchButton = document.querySelectorAll(".watchButton");
+
+    watchButton.forEach((watchButton) => {
+        let isActive = false;
+
+        watchButton.addEventListener("click", () => {
+            isActive = !isActive;
+            watchButton.src = isActive ? watchButton.dataset.active : watchButton.dataset.default;
+        });
     });
 
     const movieCard = document.querySelectorAll(".movieCard")
