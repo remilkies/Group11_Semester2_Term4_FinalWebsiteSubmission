ff38cb1463aafbccf70369037df85a4b082e14ef
Joelene241161 <joelenesuenedutoit@gmail.com>
2025-10-10 17:20:44 +0200

    Homepage information
    All data is now being displayed on the homepage, except for the user's name and the director's.

--- js/script.js
+++ js/script.js
@@ -70,8 +70,8 @@
 
     //Homepage
 class NewMovies {
-  constructor(newMovies, title, poster, director, rating){
-        this.newMovies = newMovies;
+  constructor(movieID, title, poster, director, rating){
+        this.movieID = movieID;
         this.title = title;
         this.poster = poster;
         this.director = director;
@@ -82,7 +82,7 @@ class NewMovies {
     //API request
 
     !async function(){
-const url = 'https://youtube138.p.rapidapi.com/auto-complete/?q=desp&hl=en&gl=US';
+const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
 const options = {
 	  method: 'GET',
   headers: {
@@ -96,39 +96,47 @@ let data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=
         .then((result)=> {return result})
         .catch((error)=> console.log(error));
 
+//may affect styling
+// needed to display the image: gives the API necessary information
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
         let movieList = [];
 
         for (i = 0; i < data.results.length; i++){
 
-        let newMovies = data.results[i].now_playing;
+        let movieID = data.results[i].id;
         let title = data.results[i].title;
         let poster = data.results[i].poster_path;
         let director = data.results[i].known_for_department;
         let rating = data.results[i].vote_average;
 
-        movieList.push(window["movie_" + i] = new NewMovies(newMovies, title, poster,director, rating));
+        movieList.push(window["movie_" + i] = new NewMovies(movieID, title, poster,director, rating));
     }
 
     console.log(movieList);
 
-    document.getElementById("titleFeature1").innerHTML = movieList[0].title;
-    document.getElementById("titleFeature2").innerHTML = movieList[1].title;
+    //image slider titles
+    document.getElementById("titleFeature1").innerHTML = movieList[12].title;
+    document.getElementById("titleFeature2").innerHTML = movieList[6].title;
     document.getElementById("titleFeature3").innerHTML = movieList[2].title;
     document.getElementById("titleFeature4").innerHTML = movieList[3].title;
-    
-    document.getElementById("imageFeature1").innerHTML = movieList[0].poster;
-    document.getElementById("imageFeature2").innerHTML = movieList[1].poster;
-    document.getElementById("imageFeature3").innerHTML = movieList[2].poster;
-    document.getElementById("imageFeature4").innerHTML = movieList[3].poster;
+
+    //image slider ratings
+    document.getElementById("featureRating1").innerHTML = movieList[12].rating;
+    document.getElementById("featureRating2").innerHTML = movieList[6].rating;
+    document.getElementById("featureRating3").innerHTML = movieList[2].rating;
+    document.getElementById("featureRating4").innerHTML = movieList[3].rating;
+
+    //image slider posters
+    document.getElementById("imageFeature1").src = IMAGE_BASE_URL + movieList[12].poster;
+    document.getElementById("imageFeature2").src = IMAGE_BASE_URL + movieList[6].poster;
+    document.getElementById("imageFeature3").src = IMAGE_BASE_URL + movieList[2].poster;
+    document.getElementById("imageFeature4").src = IMAGE_BASE_URL + movieList[3].poster;
 
 }();
 
-
 // Notes:
 // Get the right information for the director
-//Show the information in the on the website
-//For the next part, to show the most popular movies do I use a new async function?
 
 class PopularMovies {
   constructor(title, poster, overview){
@@ -155,6 +163,9 @@ let data = await fetch(url, options)
         .then((result)=> {return result})
         .catch((error)=> console.log(error));
 
+    //may affect styling
+    // needed to display the image: gives the API necessary information
+    const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
         let popMovies = [];
 
@@ -169,9 +180,93 @@ let data = await fetch(url, options)
 
     console.log(popMovies);
 
-    //need to display the information on the website
+    //popular movies titles
+    document.getElementById('titlePopular1').innerHTML = popMovies[0].title;
+    document.getElementById('titlePopular2').innerHTML = popMovies[1].title;
+    document.getElementById('titlePopular3').innerHTML = popMovies[17].title;
+    document.getElementById('titlePopular4').innerHTML = popMovies[3].title;
+    document.getElementById('titlePopular5').innerHTML = popMovies[18].title;
+
+    //popular movies descriptions
+    document.getElementById('Overview1').innerHTML = popMovies[0].overview;
+    document.getElementById('Overview2').innerHTML = popMovies[1].overview;
+    document.getElementById('Overview3').innerHTML = popMovies[17].overview;
+    document.getElementById('Overview4').innerHTML = popMovies[3].overview;
+    document.getElementById('Overview5').innerHTML = popMovies[18].overview;
+
+    //popular movies posters
+    document.getElementById("imagePopular1").src = IMAGE_BASE_URL + popMovies[0].poster;
+    document.getElementById("imagePopular2").src = IMAGE_BASE_URL + popMovies[1].poster;
+    document.getElementById("imagePopular3").src = IMAGE_BASE_URL + popMovies[17].poster;
+    document.getElementById("imagePopular4").src = IMAGE_BASE_URL + popMovies[3].poster;
+    document.getElementById("imagePopular5").src = IMAGE_BASE_URL + popMovies[18].poster;
     
 }();
 
-//     //get new released movies
-// }();
+//Top Rated Movies
+class TopMovies {
+  constructor(title, poster, overview){
+        this.title = title;
+        this.poster = poster;
+        this.overview = overview;
+    }
+}
+    
+    //API request
+
+    !async function(){
+const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200';
+const options = {
+	  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OWM4YWRiNmE3NGIyZDViNTA1MmE3ZjBlMTA0NDA1ZiIsIm5iZiI6MTc1ODI5Mjg1NS43NzEwMDAxLCJzdWIiOiI2OGNkNmI3NzI1NjVlMzcxOTMxNDk2NDciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.AoEE9Ow4n3Zun2dAOqNR-kWFa3MW5RQ3DWYzRGSuZOc'
+  }
+};
+
+let data = await fetch(url, options)
+        .then((response)=> response.json())
+        .then((result)=> {return result})
+        .catch((error)=> console.log(error));
+
+    //may affect styling
+    // needed to display the image: gives the API necessary information
+    const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+        let topMovies = [];
+
+        for (i = 0; i < data.results.length; i++){
+
+        let title = data.results[i].title;
+        let poster = data.results[i].poster_path;
+        let overview = data.results[i].overview;
+
+        topMovies.push(window["movie_" + i] = new TopMovies(title, poster, overview));
+    }
+
+    console.log(topMovies);
+
+    //Top Rated movies titles
+    document.getElementById('titleTop1').innerHTML = topMovies[0].title;
+    document.getElementById('titleTop2').innerHTML = topMovies[1].title;
+    document.getElementById('titleTop3').innerHTML = topMovies[17].title;
+    document.getElementById('titleTop4').innerHTML = topMovies[3].title;
+    document.getElementById('titleTop5').innerHTML = topMovies[18].title;
+
+    //Top Rated movies descriptions
+    document.getElementById('OverviewTop1').innerHTML = topMovies[0].overview;
+    document.getElementById('OverviewTop2').innerHTML = topMovies[1].overview;
+    document.getElementById('OverviewTop3').innerHTML = topMovies[17].overview;
+    document.getElementById('OverviewTop4').innerHTML = topMovies[3].overview;
+    document.getElementById('OverviewTop5').innerHTML = topMovies[18].overview;
+
+    //Top Rated movies posters
+    document.getElementById("imageTop1").src = IMAGE_BASE_URL + topMovies[0].poster;
+    document.getElementById("imageTop2").src = IMAGE_BASE_URL + topMovies[1].poster;
+    document.getElementById("imageTop3").src = IMAGE_BASE_URL + topMovies[17].poster;
+    document.getElementById("imageTop4").src = IMAGE_BASE_URL + topMovies[3].poster;
+    document.getElementById("imageTop5").src = IMAGE_BASE_URL + topMovies[18].poster;
+
+    //need to display the information on the website
+    
+}();
